0fbfd23a263b74f0b2f7633c39c7d645e31da737
Jordan Harband <ljharb@gmail.com>
2024-10-06 11:26:23 +0700

    [Deps] update `define-properties`, `es-abstract`; use `call-bind`

--- implementation.js
+++ implementation.js
@@ -2,11 +2,11 @@
 
 'use strict';
 
-var callBound = require('es-abstract/helpers/callBound')
-var RequireObjectCoercible = require('es-abstract/2019/RequireObjectCoercible');
-var ToString = require('es-abstract/2019/ToString');
-var ToInteger = require('es-abstract/2019/ToInteger');
-var IsRegExp = require('es-abstract/2019/IsRegExp');
+var callBound = require('call-bind/callBound')
+var RequireObjectCoercible = require('es-abstract/2024/RequireObjectCoercible');
+var ToString = require('es-abstract/2024/ToString');
+var ToIntegerOrInfinity = require('es-abstract/2024/ToIntegerOrInfinity');
+var IsRegExp = require('es-abstract/2024/IsRegExp');
 
 var min = Math.min;
 var max = Math.max;
@@ -21,7 +21,7 @@ module.exports = function includes(searchString) {
 	var searchStr = String(searchString);
 	var searchLength = searchStr.length;
 	var position = arguments.length > 1 ? arguments[1] : undefined;
-	var pos = ToInteger(position);
+	var pos = ToIntegerOrInfinity(position);
 	var len = S.length;
 	var start = min(max(pos, 0), len);
 	// Avoid the `indexOf` call if no match is possible
